57f52de57e1b4f5b23921ad5f5db3ebae3cf72c5
bater <jace_ka@yahoo.com.tw>
2018-10-13 15:25:01 +0900

    hello world

---
+++ day2/hello.go
@@ -0,0 +1,13 @@
+package main
+
+// 引入套件
+import (
+	"fmt"
+)
+
+// 程式執行入口
+func main() {
+	// 使用 fmt 套件印出字串 word，使用 := 簡化原本變數宣告 var word string = "Hello, World!"
+	word := "Hello, World!"
+	fmt.Println(word)
+}
